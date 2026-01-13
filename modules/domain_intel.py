7b4a5a67d4a47e4d5557d285433456396298044f
Atikul Islam Rabbi <atikulislamx25@gmail.com>
2026-01-13 21:14:52 +0600

    Update domain_intel.py

--- modules/domain_intel.py
+++ modules/domain_intel.py
@@ -1,1 +1,13 @@
-#domain_intel
+"""
+Analyze associated domains / external links
+"""
+
+def check_domain(username):
+    """
+    Args:
+        username (str)
+    Returns:
+        List[Dict]: [{"domain": "example.com", "age_days": 12, "link": "..."}]
+    """
+    # TODO: Implement WHOIS / domain age / link reputation logic
+    return []
