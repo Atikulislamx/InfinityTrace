d4532a30d52e80850d083f82785b9c1929bcfa5c
Atikul Islam Rabbi <atikulislamx25@gmail.com>
2026-01-13 21:14:33 +0600

    Update image_analysis.py

--- modules/image_analysis.py
+++ modules/image_analysis.py
@@ -1,1 +1,13 @@
-#image_analysis
+"""
+Check if profile images are reused publicly (stock sites etc.)
+"""
+
+def check_image(username):
+    """
+    Args:
+        username (str)
+    Returns:
+        List[Dict]: [{"site": "StockImage", "link": "..."}]
+    """
+    # TODO: Implement reverse image search or stock image detection
+    return []
